db85f6f2fdb8d439ec1d7b937bdd4e58a37fa78d
Alexander Hamal <hamala24@spstrutnov.cz>
2025-11-07 09:27:33 +0100

    přidán json
    tam je jak si představuju uložení divizí

---
+++ srdceZeZeleza/srdceZeZeleza/divisionDesigner.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace srdceZeZeleza;
+
+public class divisionDesigner
+{
+    private Texture2D textura;
+    
+    public int SupportSize {get; private set;}
+    public int CombatBoxSizeX {get; private set;}
+    public int CombatBoxSizeY {get; private set;}
+    
+    public string[] SupportColumn { get; private set; } //{ "+", "+", "+", "+", "+" };
+    public string[][] CombatBox { get; private set; }
+
+    public void Draw(GraphicsDeviceManager graphics, GraphicsDevice device, Color barva)
+    {
+        
+    }
+}
