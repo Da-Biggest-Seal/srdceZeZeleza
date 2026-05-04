8f5cbeebdeaa3dbfbd80e8ae78b48581cd36a9d0
Alexander Hamal <hamala24@spstrutnov.cz>
2026-05-04 09:35:25 +0200

    asdfghjkl

---
+++ srdceZeZeleza/srdceZeZeleza/battalion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace srdceZeZeleza;
+
+public class Statistics
+{
+    public int Soft { get; private set; }
+    public int Hard { get; private set; }
+    public int Air { get; private set; }
+    public int Hp { get; private set; }
+    public int Org { get; private set; }
+}
+
+public class Requirements
+{
+    public int Manpower { get; private set; }
+    public Dictionary<string, int> Equipment { get; private set; } = new Dictionary<string, int>();
+}
+
+public class Battalion
+{
+    public Statistics Stats { get; private set; }
+    public Requirements Req { get; private set; }
+}
---
+++ srdceZeZeleza/srdceZeZeleza/division.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace srdceZeZeleza;
+
+public class Division
+{
+    public string Name;
+    
+    public string[] SupportColumn = new string[5];
+    public string[,] CombatBlock = new string[5, 5];
+    
+    public Statistics Stats { get; set; } = new Statistics();
+    public Requirements Req { get; set; } = new Requirements();
+    
+    string jsonString = File.ReadAllText("battalion.json");
+    
+
+    public Division()
+    {
+        //initialize the arrays
+        for (int i = 0; i < SupportColumn.Length; i++) SupportColumn[i] = "";
+
+        for (int i = 0; i < CombatBlock.GetLength(0); i++)
+        {
+            for (int j = 0; j < CombatBlock.GetLength(1); j++)
+            {
+                CombatBlock[i, j] = "";
+            }
+        }
+    }
+}
